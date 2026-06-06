f720648d650631c2fa6f08fbaee6ca98d37cc663
kubarada <jakub.rd@seznam.cz>
2026-06-06 20:56:45 +0200

    new: setup githup pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/happy_hippos_app/',
   plugins: [react()],
 })
